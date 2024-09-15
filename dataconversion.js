b719d0078f1833ca46f73cc4470c67b984fa0d01
Janadriel <pjanadriel@gmail.com>
2024-09-15 13:14:31 +0800

    Create dataconversion.js

---
+++ dataconversion.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let j = parseInt(price);
+console.log("Using parseInt():", j);
+let d = Number(price);
+console.log("Using Number():", d);
+let e = +price;
+console.log("Using + operator:", e);
+
+
+let distance = 150;
+let q = distance.toString();
+console.log("Using toString():", q);
+let r = String(distance);
+console.log("Using String():", r);
+
+
+let weight = "72.5";
+let h = parseFloat(weight);
+console.log("Using parseFloat():", h);
